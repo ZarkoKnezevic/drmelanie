59f12054976fd259d9c25af223761dde13119759
knezevicz <zarkoknezevic@lx-media.at>
2025-11-30 21:06:42 +0100

    tailiwind confog refactor

--- storyblok-app/tailwind.config.ts
+++ storyblok-app/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss';
+import { fontStylePreset } from './src/components/ui/presets/fontStylePreset';
+import { colorPreset } from './src/components/ui/presets/colorPreset';
 
 export default {
   darkMode: ['class'],
+  presets: [fontStylePreset, colorPreset],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -28,91 +31,8 @@ export default {
       padding: '2rem',
     },
     extend: {
-      fontFamily: {
-        karla: ['var(--font-karla)', 'sans-serif'],
-      },
-      colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
-        background: {
-          DEFAULT: 'var(--background)',
-          primary: 'var(--background-primary)',
-          secondary: 'var(--background-secondary)',
-          tertiary: 'var(--background-tertiary)',
-          quaternary: 'var(--background-quaternary)',
-        },
-        button: {
-          DEFAULT: 'var(--button)',
-          primary: {
-            DEFAULT: 'var(--button-primary)',
-            text: 'var(--button-primary-foreground)',
-            border: 'var(--button-primary-foreground)',
-            hover: 'var(--button-primary-hover)',
-          },
-          secondary: {
-            DEFAULT: 'var(--button-secondary)',
-            text: 'var(--button-secondary-foreground)',
-            border: 'var(--button-secondary-foreground)',
-            hover: 'var(--button-secondary-hover)',
-          },
-          tertiary: {
-            DEFAULT: 'var(--button-tertiary)',
-            text: 'var(--button-tertiary-foreground)',
-            border: 'var(--button-tertiary-foreground)',
-            hover: 'var(--button-tertiary-hover)',
-          },
-          quaternary: {
-            DEFAULT: 'var(--button-quaternary)',
-            text: 'var(--button-quaternary-foreground)',
-            border: 'var(--button-quaternary-foreground)',
-            hover: 'var(--button-quaternary-hover)',
-          },
-        },
-        foreground: 'var(--foreground)',
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
-        },
-        tertiary: {
-          DEFAULT: 'var(--tertiary)',
-          foreground: 'var(--tertiary-foreground)',
-        },
-        quaternary: {
-          DEFAULT: 'var(--quaternary)',
-          foreground: 'var(--quaternary-foreground)',
-        },
-        popover: {
-          DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
-        },
-        card: {
-          DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+      maxWidth: {
+        container: '104rem', //1664px
       },
     },
   },
